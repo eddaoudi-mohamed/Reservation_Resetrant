338d4ed9b4540effa0eaab8e71e33e2baae75331
eddaoudi-mohamed <daoudi6745mohamed@gmail.com>
2024-03-30 08:47:04 +0000

    add some feature side admin

---
+++ resources/views/Admin/Category/create.blade.php
@@ -0,0 +1,25 @@
+<x-admin-layout>
+    <x-slot name="content">
+      <form action="" method="post" class="w-full container md:w-2/3 mx-auto h-fit my-auto"> 
+        @csrf
+        <h1  class=" text-white mb-4 text-xl my-2 text-center font-bold">Create Category</h1>
+        <div class="mb-5">
+            <label for="name" class="block mb-4 font-semibold text-sm  text-gray-900 dark:text-white">Name</label>
+            <input type="text" id="name" name="name" class="bg-gray-50 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> Username already taken!</p> </div>
+
+        <div class="mb-5">
+            <label for="description" class="block mb-4 font-semibold text-sm  text-gray-900 dark:text-white">Description</label>
+            <input type="text" id="description"  name="description" class="bg-gray-50 h-14 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </div>
+
+        <div class="mb-5">
+            <label for="image" class="block mb-4 font-semibold text-sm  text-gray-900 dark:text-white">Uploade Image</label>
+            <input  id="image" name="image" class="block w-full  text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 p-2.5 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file">
+            <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF</p>
+        </div>
+
+        <button type="submit" class="p-2.5 text-sm font-medium w-full bg-indigo-500 hover:bg-indigo-700 text-lime-50 rounded-lg" >Create Category</button>
+     </form>
+    </x-slot>
+</x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,23 +14,62 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body  class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <body  class="font-sans antialiased min-h-screen">
+        <div class="min-h-screen flex gap-4 bg-gray-100 dark:bg-gray-900">
+            {{-- @include('layouts.navigation') --}}             
+            <aside id="default-sidebar" class="top-0  w-[296px] left-0 hidden md:block  h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+                <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+                <ul class="space-y-2 font-medium">
+                    <li>
+                      
+                        <x-nav-link-admin :href="route('admin.index')" :active="request()->routeIs('admin.index')">
+                            <span class="flex-1 ms-3 whitespace-nowrap">Home</span>
+                        </x-nav-link-admin>   
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+                  
+                    </li>
+                    <li>
+                        <x-nav-link-admin :href="route('admin.categories.index')" :active="request()->routeIs('admin.categories.index')">
+                            <span class="flex-1 ms-3 whitespace-nowrap">Categories</span>
+                        </x-nav-link-admin>    
+                    </li>
+                    <li>
+                        <x-nav-link-admin :href="route('admin.menus.index')" :active="request()->routeIs('admin.menus.index')">
+                            <span class="flex-1 ms-3 whitespace-nowrap">Menus</span>
+                        </x-nav-link-admin>
+                    </li>
+                    <li>
+                        <x-nav-link-admin :href="route('admin.tables.index')" :active="request()->routeIs('admin.tables.index')">
+                            <span class="flex-1 ms-3 whitespace-nowrap">Tables</span>
+                        </x-nav-link-admin>
+                  
+                    </li>
+                    <li>
+                        <x-nav-link-admin :href="route('admin.resarvations.index')" :active="request()->routeIs('admin.resarvations.index')">
+                            <span class="flex-1 ms-3 whitespace-nowrap">Resarvations</span>
+                        </x-nav-link-admin>
+                      
+                    </li>
+                    <li>
+                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            {{-- <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
+                            </svg> --}}
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="flex-1 ms-3 whitespace-nowrap" type="submit">Log Out</button>
+                            </form>
+                        </a>
+                    </li>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                </ul>
+                </div>
+            </aside>
+            <div class=" mr-8 mt-8  p-4  w-full h-screen ">
+                @if (isset($content))
+                {{ $content }}
+                @endif
+            </div>
         </div>
     </body>
 </html>
